cc7e142deac69090dcc70b2c8dd95b4212571a44
Sam <smcarthur000@gmail.com>
2022-02-21 23:47:14 -0600

    Added hello world

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,6 +1,8 @@
 package;
 
+import flixel.FlxObject;
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
@@ -9,6 +11,12 @@ class PlayState extends FlxState
 		super.create();
 		var mainPlayer = new Hero(300, 300);
 		add(mainPlayer);
+		var hellowWorld = new FlxText(200, 200, 0, "Hello world", 64);
+		hellowWorld.setBorderStyle(OUTLINE, 0x00ff00, 20, 20);
+		hellowWorld.bold = true;
+		hellowWorld.angle = 54;
+		hellowWorld.color = 0xff0000;
+		add(hellowWorld);
 	}
 
 	override public function update(elapsed:Float)
