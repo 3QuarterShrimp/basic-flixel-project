abc4d2cf7c12389ca075b2c16e5cc922177fe728
Sam <smcarthur000@gmail.com>
2022-02-23 01:33:29 -0600

    Fixed based on suggestions

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(1000, 500, PlayState));
+		addChild(new FlxGame(640, 480, PlayState));
 	}
 }
--- source/ScreensaverHero.hx
+++ source/ScreensaverHero.hx
@@ -3,76 +3,56 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.math.FlxRandom;
+import flixel.math.FlxVelocity;
+import flixel.util.FlxColor;
 import flixel.util.FlxSpriteUtil;
 
 class ScreensaverHero extends FlxSprite
 {
-	public var direction:Int;
+	public var direction:Float;
+	public var randomDirection:FlxRandom = new FlxRandom();
 
 	public function new(x:Int, y:Int)
 	{
 		super(x, y);
 		var randomDirection:FlxRandom = new FlxRandom();
-		this.direction = randomDirection.int(0, 7);
+		this.makeGraphic(25, 25, FlxColor.WHITE);
+		this.direction = randomDirection.float(0, 6.28318530718);
+		randDirectionMove(this.direction);
+	}
+
+	public function randDirectionMove(direction)
+	{
+		FlxVelocity.accelerateFromAngle(this, direction, 25, 50, true);
+	}
+
+	public function colorChange(x:Float, y:Float)
+	{
+		if (x > y)
+		{
+			this.color = 0xff0000;
+		}
+		if (x < y)
+		{
+			this.color = 0x0000ff;
+		}
+		else
+		{
+			this.color = 0x00ff00;
+		}
 	}
 
 	override function update(elapsed:Float)
 	{
-		if (this.x < 500)
-		{
-			this.color = 0xff0000;
-		}
-		if (this.x > 500)
-		{
-			this.color = 0x00ff00;
-		}
-		if (this.direction == 0)
-		{
-			this.x--;
-		}
-		else if (this.direction == 1)
-		{
-			this.x--;
-			this.y--;
-		}
-		else if (this.direction == 2)
-		{
-			this.y--;
-		}
-		else if (this.direction == 3)
-		{
-			this.x++;
-		}
-		else if (this.direction == 4)
-		{
-			this.x++;
-			this.y++;
-		}
-		else if (this.direction == 5)
-		{
-			this.y++;
-		}
-		else if (this.direction == 6)
-		{
-			this.x--;
-			this.y++;
-		}
-		else
-		{
-			this.x++;
-			this.y--;
-		}
+		colorChange(this.x / FlxG.width, this.y / FlxG.height);
 		if (FlxG.mouse.pressed)
 		{
-			this.direction++;
-			if (this.direction == 8)
-			{
-				this.direction = 0;
-			}
+			var randomDirection:FlxRandom = new FlxRandom();
+			randDirectionMove(randomDirection.float(0, 6.28318530718));
 		}
 		if (this.x <= 0)
 		{
-			this.x = 1000;
+			this.x = FlxG.width;
 		}
 		else if (this.x >= FlxG.width)
 		{
@@ -80,7 +60,7 @@ class ScreensaverHero extends FlxSprite
 		}
 		if (this.y <= 0)
 		{
-			this.y = 500;
+			this.y = FlxG.height;
 		}
 		else if (this.y >= FlxG.height)
 		{
